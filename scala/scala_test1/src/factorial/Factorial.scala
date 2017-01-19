82a228366f8d5afa2a280e3b7efe447afc10bc69
camiloh <camiloahm@gmail.com>
2017-01-18 23:10:11 -0500

    scala pure and impure

---
+++ scala/scala_test1/src/factorial/Factorial.scala
@@ -0,0 +1,25 @@
+package factorial
+
+import scala.annotation.tailrec
+
+/**
+  * Created by CamiloH on 1/18/2017.
+  */
+object Factorial {
+
+  def main(args: Array[String]): Unit = {
+    print(factorial(3))
+    print(factorial(7))
+  }
+
+  private def factorial(n: Int): Int = {
+    @tailrec
+    def go(n: Int, acc: Int): Int = {
+      if (n <= 0) acc
+      else go(n - 1, n * acc)
+    }
+
+    go(n, 1)
+  }
+
+}
