7fb96ea7039805484d13e6fa2b7c89701d2b6af0
Camilo Andrés Hernández Marin <camiloahm@gmail.com>
2017-01-24 20:01:24 -0500

    Chapter 2 implicit functions

--- scala/fpinscala/src/test/scala-2.12/com/fpin/Chapter2Test.scala
+++ scala/fpinscala/src/test/scala-2.12/com/fpin/Chapter2Test.scala
@@ -165,4 +165,37 @@ class Chapter2Test extends FreeSpec with Matchers {
       isBalanced("(3+(1+2))") should be(true)
     }
   }
+
+
+  def isSorted[A](as: Array[A], gt: (A, A) => Boolean): Boolean = {
+    @annotation.tailrec
+    def go(n: Int): Boolean = {
+      if (n >= as.length - 1)
+        true
+      else if (gt(as(n), as(n + 1)))
+        false
+      else
+        go(n + 1)
+    }
+
+    go(0)
+  }
+
+
+  def partial1[A, B, C](a: A, f: (A, B) => C): B => C = {
+    b => f(a, b)
+  }
+
+  def curry[A, B, C](f: (A, B) => C): A => (B => C) = {
+    a => b => f(a, b)
+  }
+
+  def uncurry[A, B, C](f: A => B => C): (A, B) => C = {
+    (a, b) => f(a)(b)
+  }
+
+  def compose[A, B, C](f: B => C, g: A => B): A => C = {
+    a => f(g(a))
+  }
+
 }
