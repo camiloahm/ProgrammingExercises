3c73fcc5b6399b13aaf48c52ed6144d9eb5d44a9
Camilo Andrés Hernández Marin <camiloahm@gmail.com>
2017-02-20 17:12:20 -0500

    Key vault experiment

---
+++ azure/azurekeyvault/Startup.cs
@@ -0,0 +1,55 @@
+using System.IO;
+using System.Text;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace azurekeyvault
+{
+    public class Startup
+    {
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                   .SetBasePath(Directory.GetCurrentDirectory())
+                   .AddJsonFile("appsettings.json", optional: false)
+                   .AddEnvironmentVariables();
+
+            var config = builder.Build();
+
+            builder.AddAzureKeyVault(
+                    $"https://{config["Vault"]}.vault.azure.net/",
+                    config["ClientId"],
+                    config["ClientSecret"]);
+
+            Configuration = builder.Build();
+
+        }
+
+        public IConfigurationRoot Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Add framework services.
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            app.Run(async context =>
+            {
+                var encoding = new UTF8Encoding(encoderShouldEmitUTF8Identifier: false);
+                var document = string.Format(Configuration["MySecret"], Configuration["Section:MySecret"], Configuration.GetSection("Section")["MySecret"]);
+                context.Response.ContentLength = encoding.GetByteCount(document);
+                context.Response.ContentType = "text/html";
+                await context.Response.WriteAsync(document);
+            });
+            app.UseMvc();
+        }
+    }
+}
